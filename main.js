3c12089a53a64c17162b082309056c8029254f22
barba <sandrolini.barbara@nebbialab.it>
2025-01-14 23:50:54 +0100

    First working cube w/ sky

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import {OrbitControls} from "three/addons";
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0x49bce3);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
@@ -27,7 +28,17 @@ window.addEventListener('resize', function() {
 const orbit = new OrbitControls(camera, renderer.domElement)
 orbit.update();
 
-// MESHES
+//RAYCASTER
+const raycaster = new THREE.Raycaster();
+
+//MOUSE POSITION TRACKING + POSITION NORMLIZATION
+const mousePosition = new THREE.Vector2();
+window.addEventListener('mousemove', function(e) {
+    mousePosition.x = (e.clientX/window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY/window.innerHeight) * 2 + 1;
+});
+
+//MESHES
 const boxGeometry = new THREE.BoxGeometry(1,1,1);
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
