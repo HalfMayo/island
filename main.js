50ae4785fad9d56a800ced58029f2167820be21a
barba <sandrolini.barbara@nebbialab.it>
2025-01-22 20:17:56 +0100

    Scene change now works

--- main.js
+++ main.js
@@ -23,12 +23,10 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setClearColor(0x49bce3);
 document.getElementById('canvas').appendChild(renderer.domElement);
 
-//ANTI-ALIAS EFFECT POST-PROCESS SHADER
-let effectFXAA;
-
 //SCENES
 const scene = new THREE.Scene();
 const sceneCube = new THREE.Scene();
+let sceneToDisplay = scene;
 
 //CAMERAS
 const camera = new THREE.PerspectiveCamera(
@@ -39,22 +37,36 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.set(0, 25, -30);
 
-const cameraCube = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    1000
-)
-cameraCube.position.set(0, 35, -30);
+//PP VARIABLES
+let selectedObjs = [];
+let renderPass, outlinePass, outputPass, effectFXAA;
 
 //EVENT LISTENERS
 window.addEventListener('resize', function() {
     camera.aspect = window.innerWidth/window.innerHeight;
     camera.updateProjectionMatrix();
-    cameraCube.aspect = window.innerWidth/window.innerHeight;
-    cameraCube.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     effectFXAA.uniforms['resolution'].value.set(1/window.innerWidth, 1/window.innerHeight);
+})
+
+document.getElementById('canvas').addEventListener('click', () => {
+    if(selectedObjs.length > 0) {
+        console.log('quaso', selectedObjs[0].name);
+        sceneToDisplay = sceneCube;
+        outlinePass.selectedObjects = [];
+        hidePlaceDescription(meshes);
+        launchPostProcessing(composer);
+        document.getElementById('back-button').classList.remove('hidden')
+    }
+})
+
+document.getElementById('back-button').addEventListener('click', () => {
+    console.log('quaso');
+    sceneToDisplay = scene;
+    outlinePass.selectedObjects = [];
+    hidePlaceDescription(meshes);
+    launchPostProcessing(composer);
+    document.getElementById('back-button').classList.add('hidden');
 })
 
 renderer.domElement.addEventListener('pointermove', onPointerMove)
@@ -65,8 +77,6 @@ renderer.domElement.addEventListener('pointermove', onPointerMove)
 //ORBIT CONTROLS
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.maxPolarAngle = Math.PI / 2;
-const orbitCube = new OrbitControls(cameraCube, renderer.domElement);
-orbitCube.maxPolarAngle = Math.PI / 2;
 
 //RAYCASTER
 const raycaster = new THREE.Raycaster();
@@ -74,13 +84,6 @@ const raycaster = new THREE.Raycaster();
 //MOUSE POSITION
 const mousePosition = new THREE.Vector2();
 
-//SELECTED OBJS ARRAY
-let selectedObjs = [];
-window.addEventListener('click', () => {
-    if(selectedObjs.length > 0) {
-        console.log(selectedObjs[0].name);
-    }
-})
 
 // MISE-EN-SCENE //
 
@@ -100,6 +103,7 @@ const meshes = ['ocean', 'island', 'beach', 'lighthouse', 'church', 'houses', 'f
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.rotation.set(3, 5, 0);
 sceneCube.add(box);
 
 //LIGHTS
@@ -120,23 +124,7 @@ sceneCube.add(directionalCube);
 
 //OUTLINE PP
 const composer = new EffectComposer(renderer);
-
-const renderPass = new RenderPass(scene, camera);
-composer.addPass(renderPass);
-
-const outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, camera);
-outlinePass.hiddenEdgeColor.set(new THREE.Color().setHex( 0xffffff ));
-outlinePass.edgeStrength = 7.5;
-outlinePass.edgeGlow = 0;
-outlinePass.edgeThickness = 1.5;
-composer.addPass(outlinePass);
-
-const outputPass = new OutputPass();
-composer.addPass(outputPass);
-
-effectFXAA = new ShaderPass(FXAAShader);
-effectFXAA.uniforms['resolution'].value.set(1/window.innerWidth, 1/window.innerHeight);
-composer.addPass(effectFXAA);
+launchPostProcessing(composer);
 
 
 // ANIMATION LAUNCH //
@@ -152,43 +140,45 @@ function onPointerMove(event) {
     mousePosition.x = (event.clientX/window.innerWidth) * 2 - 1;
     mousePosition.y = -(event.clientY/window.innerHeight) * 2 + 1;
     //RAYCASTING
-    raycaster.setFromCamera(mousePosition, camera);
-    const intersectedObjs = raycaster.intersectObjects(scene.children);
-    if (intersectedObjs.length > 0) {
-        selectedObjs = [];
-        selectedObjs.push(intersectedObjs[0].object);
-        outlinePass.selectedObjects = selectedObjs;
-        switch(intersectedObjs[0]?.object.name) {
-            case 'ocean':
-                showPlaceDescription( 'ocean');
-                break;
-            case 'island':
-                showPlaceDescription( 'island');
-                break;
-            case 'beach':
-                showPlaceDescription( 'beach');
-                break;
-            case 'lighthouse':
-                showPlaceDescription( 'lighthouse');
-                break;
-            case 'church':
-                showPlaceDescription( 'church');
-                break;
-            case 'houses':
-                showPlaceDescription( 'houses');
-                break;
-            case 'fishermenVillage':
-                showPlaceDescription( 'fishermenVillage');
-                break;
-            case 'dock':
-                showPlaceDescription( 'dock');
-                break;
-            default:
-                break;
+    if(sceneToDisplay === scene) {
+        raycaster.setFromCamera(mousePosition, camera);
+        const intersectedObjs = raycaster.intersectObjects(scene.children);
+        if (intersectedObjs.length > 0) {
+            selectedObjs = [];
+            selectedObjs.push(intersectedObjs[0].object);
+            outlinePass.selectedObjects = selectedObjs;
+            switch(intersectedObjs[0]?.object.name) {
+                case 'ocean':
+                    showPlaceDescription( 'ocean');
+                    break;
+                case 'island':
+                    showPlaceDescription( 'island');
+                    break;
+                case 'beach':
+                    showPlaceDescription( 'beach');
+                    break;
+                case 'lighthouse':
+                    showPlaceDescription( 'lighthouse');
+                    break;
+                case 'church':
+                    showPlaceDescription( 'church');
+                    break;
+                case 'houses':
+                    showPlaceDescription( 'houses');
+                    break;
+                case 'fishermenVillage':
+                    showPlaceDescription( 'fishermenVillage');
+                    break;
+                case 'dock':
+                    showPlaceDescription( 'dock');
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            outlinePass.selectedObjects = [];
+            hidePlaceDescription(meshes);
         }
-    } else {
-        outlinePass.selectedObjects = [];
-        hidePlaceDescription(meshes);
     }
 }
 
@@ -204,7 +194,25 @@ function hidePlaceDescription(arr) {
     })
 }
 
+function launchPostProcessing(composer) {
+    renderPass = new RenderPass(sceneToDisplay, camera);
+    composer.addPass(renderPass);
+
+    outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), sceneToDisplay, camera);
+    outlinePass.hiddenEdgeColor.set(new THREE.Color().setHex( 0xffffff ));
+    outlinePass.edgeStrength = 7.5;
+    outlinePass.edgeGlow = 0;
+    outlinePass.edgeThickness = 1.5;
+    composer.addPass(outlinePass);
+
+    outputPass = new OutputPass();
+    composer.addPass(outputPass);
+
+    effectFXAA = new ShaderPass(FXAAShader);
+    effectFXAA.uniforms['resolution'].value.set(1/window.innerWidth, 1/window.innerHeight);
+    composer.addPass(effectFXAA);
+}
+
 function animate() {
-    composer.render(scene, camera);
-    // renderer.render(scene, camera)
+    composer.render(sceneToDisplay, camera);
 }
